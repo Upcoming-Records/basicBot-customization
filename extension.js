fa1e98c23e6f2b6a053a0b6f1c50ac94fc2190b6
RobGoldy <nine5one@gmail.com>
2015-08-30 01:21:30 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -49,7 +49,7 @@
       botName: "Z00P",
       language: "english",
       chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+      scriptLink: "https://rawgit.com/Upcoming-Records/basicBot/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
